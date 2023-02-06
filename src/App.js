298ed57029074c5667bed0cfa981236ee95e913d
WatkornFeng <watcharakorn2022@gmail.com>
2023-02-06 21:23:41 +0700

    move webapi code to api folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import rateApi from "./api/rateApi";
 import PagePrice from "./components/PagePrice";
 import PageDetailPrice from "./components/PageDetailPrice";
 import PageTransaction from "./components/PageTransaction";
@@ -9,57 +10,15 @@ import PageLogin from "./components/PageLogin";
 import PageCreateAcc from "./components/PageCreateAcc";
 import "./App.css";
 
-function App4() {
+function App() {
   const [price, setPrice] = useState([]);
-  const apiKey = process.env.REACT_APP_API_KEY;
-  const requestOptions = {
-    method: "GET",
-    redirect: "follow",
-    headers: {
-      "X-IBM-Client-Id": apiKey,
-    },
-  };
-  const url =
-    "https://apigw1.bot.or.th/bot/public/Stat-ExchangeRate/v2/DAILY_AVG_EXG_RATE/?";
-  const yesterday = new Date();
+  async function callApi() {
+    const data = await rateApi(price);
+    setPrice(data);
+  }
 
   useEffect(() => {
-    getPrice();
-    async function getPrice() {
-      let i = 0;
-      if (price !== "") {
-        while (true) {
-          i++;
-          yesterday.setDate(yesterday.getDate() - i);
-          function padTo2Digits(num) {
-            return num.toString().padStart(2, "0");
-          }
-          function formatDate(date) {
-            return [
-              date.getFullYear(),
-              padTo2Digits(date.getMonth() + 1),
-              padTo2Digits(date.getDate()),
-            ].join("-");
-          }
-          const previousDay = formatDate(yesterday);
-          console.log(previousDay);
-          const start_period = `start_period=${previousDay}`;
-          const end_period = `&end_period=${previousDay}`;
-          const api = url + start_period + end_period;
-
-          const response = await fetch(api, requestOptions);
-          const responseJson = await response.json();
-          const data = responseJson.result.data.data_detail[0].period;
-
-          if (data !== "") {
-            setPrice(responseJson.result);
-            break;
-          }
-        }
-      } else {
-        console.log("emply at 1st time");
-      }
-    }
+    callApi();
   }, []);
 
   return (
@@ -79,4 +38,4 @@ function App4() {
     </>
   );
 }
-export default App4;
+export default App;
---
+++ src/api/RateApi.js
@@ -0,0 +1,42 @@
+const apiKey = process.env.REACT_APP_API_KEY;
+const requestOptions = {
+  method: "GET",
+  redirect: "follow",
+  headers: {
+    "X-IBM-Client-Id": apiKey,
+  },
+};
+export const url =
+  "https://apigw1.bot.or.th/bot/public/Stat-ExchangeRate/v2/DAILY_AVG_EXG_RATE/?";
+const yesterday = new Date();
+
+export default async function rateApi(price) {
+  if (price !== "") {
+    while (true) {
+      console.log("ds" + yesterday);
+      yesterday.setDate(yesterday.getDate() - 1);
+      console.log(yesterday);
+      function padTo2Digits(num) {
+        return num.toString().padStart(2, "0");
+      }
+      function formatDate(date) {
+        return [
+          date.getFullYear(),
+          padTo2Digits(date.getMonth() + 1),
+          padTo2Digits(date.getDate()),
+        ].join("-");
+      }
+      const previousDay = formatDate(yesterday);
+      const start_period = `start_period=${previousDay}`;
+      const end_period = `&end_period=${previousDay}`;
+      const api = url + start_period + end_period;
+
+      const response = await fetch(api, requestOptions);
+      const responseJson = await response.json();
+      const hasData = responseJson.result.data.data_detail[0].period;
+      if (hasData !== "") {
+        return responseJson.result;
+      }
+    }
+  }
+}
